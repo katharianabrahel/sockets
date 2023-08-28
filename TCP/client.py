6fff64c5ca39ae43c2faa413143c5d1987bdc551
Katharian Abrahel <katharian.abrahel@gmail.com>
2023-08-28 10:45:46 -0300

    first commit

---
+++ TCP/client.py
@@ -0,0 +1,43 @@
+import socket
+
+def enviar_operacao(client_socket, operacao):
+    client_socket.send(operacao.encode())
+    response = client_socket.recv(1024).decode()
+    print(response)
+
+
+HOST = '127.0.0.1'
+PORT = 12345
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+
+while True:
+    print("Escolha uma operação:")
+    print("1. Cadastrar aluno")
+    print("2. Procurar aluno")
+    print("3. Listar alunos")
+    print("4. Sair")
+
+    escolha = input("Digite o número da operação desejada: ")
+
+    if escolha == "1":
+        nome = input("Digite o nome do aluno: ")
+        notas = input("Digite as notas separadas por espaço (ex: 7.5 8.0): ")
+        enviar_operacao(client_socket, "cadastrar")
+        client_socket.send(nome.encode())
+        client_socket.send(notas.encode())
+    elif escolha == "2":
+        nome = input("Digite o nome do aluno que deseja procurar: ")
+        enviar_operacao(client_socket, "procurar")
+        client_socket.send(nome.encode())
+    elif escolha == "3":
+        enviar_operacao(client_socket, "listar")
+    elif escolha == "4":
+        enviar_operacao(client_socket, "sair")
+        break
+    else:
+        print("Operação inválida. Tente novamente.")
+
+# Fecha a conexão com o servidor
+client_socket.close()
---
+++ TCP/server.py
@@ -0,0 +1,66 @@
+import socket
+
+def calcular_status(notas):
+    media = sum(notas) / len(notas)
+    if media >= 7.0:
+        return "Aprovado"
+    else:
+        return "Reprovado"
+
+
+def cadastrar_aluno(client_socket):
+    nome = client_socket.recv(1024).decode()
+    notas = [float(nota) for nota in client_socket.recv(1024).decode().split()]
+    status = calcular_status(notas)
+    alunos[nome] = {'notas': notas, 'status': status}
+    return f"Aluno {nome} cadastrado com sucesso."
+
+
+def procurar_aluno(client_socket):
+    nome = client_socket.recv(1024).decode()
+    if nome in alunos:
+        aluno = alunos[nome]
+        return f"Nome: {nome}, Notas: {aluno['notas']}, Média: {sum(aluno['notas']) / len(aluno['notas'])}, Status: {aluno['status']}"
+    else:
+        return f"Aluno {nome} não encontrado."
+
+
+def listar_alunos():
+    lista = []
+    for nome, aluno in alunos.items():
+        lista.append(f"Nome: {nome}, Notas: {aluno['notas']}, Média: {sum(aluno['notas']) / len(aluno['notas'])}, Status: {aluno['status']}")
+    return "\n".join(lista)
+
+alunos = {}
+
+HOST = '127.0.0.1'
+PORT = 12345  
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen(1)
+print(f"O servidor está pronto")
+
+client_socket, addr = server_socket.accept()
+print(f"Conexão recebida de {addr}")
+
+while True:
+    operacao = client_socket.recv(1024).decode()
+    
+    if operacao == "cadastrar":
+        response = cadastrar_aluno(client_socket)
+    elif operacao == "procurar":
+        response = procurar_aluno(client_socket)
+    elif operacao == "listar":
+        response = listar_alunos()
+    elif operacao == "sair":
+        response = "Encerrando a conexão."
+        client_socket.send(response.encode())
+        break
+    else:
+        response = "Operação inválida."
+
+    client_socket.send(response.encode())
+client_socket.close()
+
+server_socket.close()
