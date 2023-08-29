c6b43eada17e76623fe4375d9ff75ea6ebcfec04
Katharian Abrahel <katharian.abrahel@gmail.com>
2023-08-29 11:49:23 -0300

    adicionando aplicação UDP

---
+++ UDP/client.py
@@ -0,0 +1,40 @@
+import socket
+
+def enviar_solicitação(mensagem):
+    client_socket.sendto(mensagem.encode(), (host, port))
+    resposta, addr = client_socket.recvfrom(1024)
+    return resposta
+
+
+host = '127.0.0.1'
+port = 12345
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+while True:
+    print("Escolha uma operação:")
+    print("1. Cadastrar aluno")
+    print("2. Procurar aluno")
+    print("3. Listar alunos")
+    print("4. Sair")
+
+    escolha = input("Digite o número da operação desejada: ")
+
+    if escolha == "1":
+        nome = input("Nome do aluno: ")
+        nota1 = input("Nota 1: ")
+        nota2 = input("Nota 2: ")
+        solicitacao = f"Cadastrar {nome} {nota1} {nota2}"
+        print(enviar_solicitação(solicitacao).decode())
+    
+    elif escolha == "2":
+        nome = input("Nome do aluno: ")
+        solicitacao = f"Procurar {nome}"
+        print(enviar_solicitação(solicitacao).decode())
+    
+    elif escolha == "3":
+        solicitacao = "Listar"
+        print(enviar_solicitação(solicitacao).decode())
+
+    else:
+        print("Opção inválida")
---
+++ UDP/server.py
@@ -0,0 +1,46 @@
+import socket
+
+def calcular_status(media):
+    if media >= 7.0:
+        return "Aprovado"
+    return "Reprovado"
+
+host = '127.0.0.1'
+port = 12345
+
+alunos = {}
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind((host,port))
+
+print("O servidor UDP está pronto")
+
+while True:
+    message, addr = server_socket.recvfrom(1024)
+    message = message.decode()
+
+    if message.startswith("Cadastrar"):
+        _, nome, nota1, nota2 = message.split()
+        nota1, nota2 = float(nota1), float(nota2)
+        media = (nota1 + nota2)/2
+        status = calcular_status(media)
+        alunos[nome] = {"Nota1": nota1, "Nota2": nota2, "Média": media, "Status": status}
+        resposta = f"Aluno {nome} cadastrado com média {media:.2f} - {status}"
+
+    elif message.startswith("Procurar"):
+        _, nome = message.split()
+        aluno = alunos.get(nome, None)
+        if aluno:
+            resposta = f"Nome: {nome}, Média: {aluno['Média']:.2f}, Status: {aluno['Status']}"
+        else:
+            resposta = f"Aluno {nome} não encontrado"
+
+    elif message == "Listar":
+        resposta = ""
+        for nome, info in alunos.items():
+            resposta += f"Nome: {nome}, Média: {info['Média']:.2f}, Status: {info['Status']}\n"
+        
+    else:
+        resposta = "Comando inválido"
+
+    server_socket.sendto(resposta.encode(), addr)
